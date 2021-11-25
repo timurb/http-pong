39319267cfba09425aee82cac3439ce2bb246a12
Timur Batyrshin <erthad@gmail.com>
2021-11-25 23:13:46 +0300

    add path to dump environment

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 from flask import request
+import os
+import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return str(request.headers) + "\n" + str(request.data)
+    return str(request.headers) + "\n" + str(request.data) 
+
+@app.route("/env")
+def env():
+    return str(json.dumps(dict(os.environ), indent=2))
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
